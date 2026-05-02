28692630eb298b87f238d278c46546d82d53b4f0
Vladimir Voitenko <vladimirdev635@gmail.com>
2026-05-02 23:25:56 +0500

    fix

--- train_svm.py
+++ train_svm.py
@@ -12,7 +12,7 @@ from shared.prepared_dataset import load_prepared_dataset
 def main() -> None:
     train_inputs, train_outputs = load_prepared_dataset("train")
     test_inputs, test_outputs = load_prepared_dataset("test")
-    svc = LinearSVC(C=0.01, random_state=42)
+    svc = LinearSVC(C=0.1, random_state=42)
     svc.fit(train_inputs, train_outputs)
     predicted_outputs = svc.predict(test_inputs)
     np.save("weights.npy", svc.coef_)
